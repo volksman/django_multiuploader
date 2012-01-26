38b337c60ddbe153afca8312f0a13d686f291aee
Shayne <shayne@nomad.ca>
2012-01-26 00:54:42 +0000

    lots of changes:
    - multi model
    - removed image gallery and thusly sorl deps
    - broke it. broke it bad

--- django_multiuploader/multiuploader/templatetags/multiuploader.py
+++ django_multiuploader/multiuploader/templatetags/multiuploader.py
@@ -3,6 +3,14 @@ from django.conf import settings
 
 register = template.Library()
 
-@register.inclusion_tag('multiuploader/multiuploader_main.html')
-def multiupform():
-    return {'static_url':settings.MEDIA_URL,}
+@register.simple_tag()
+def multiupform(value):
+    try:
+        app, model = value.split('.')
+    except:
+        raise ValueError('Value should be an app.model name')
+
+    t = template.loader.select_template(['multiuploader/%s_%s.html' % (app, model), 'multiuploader/base.html'])
+    c = template.Context({ 'STATIC_URL':settings.STATIC_URL, 'model': model })
+
+    return t.render(c)
--- django_multiuploader/multiuploader/views.py
+++ django_multiuploader/multiuploader/views.py
@@ -1,15 +1,15 @@
 from django.shortcuts import get_object_or_404, render_to_response
 from django.conf import settings
 from django.http import HttpResponse, HttpResponseBadRequest
-from models import MultiuploaderImage
 from django.core.files.uploadedfile import UploadedFile
+from django.db.models.loading import get_model
 
 #importing json parser to generate jQuery plugin friendly json response
 from django.utils import simplejson
 
 #for generating thumbnails
 #sorl-thumbnails must be installed and properly configured
-from sorl.thumbnail import get_thumbnail
+#from sorl.thumbnail import get_thumbnail
 
 
 from django.views.decorators.csrf import csrf_exempt
@@ -17,25 +17,25 @@ from django.views.decorators.csrf import csrf_exempt
 import logging
 log = logging
 
-@csrf_exempt
-def multiuploader_delete(request, pk):
-    """
-    View for deleting photos with multiuploader AJAX plugin.
-    made from api on:
-    https://github.com/blueimp/jQuery-File-Upload
-    """
-    if request.method == 'POST':
-        log.info('Called delete image. image id='+str(pk))
-        image = get_object_or_404(MultiuploaderImage, pk=pk)
-        image.delete()
-        log.info('DONE. Deleted photo id='+str(pk))
-        return HttpResponse(str(pk))
-    else:
-        log.info('Received not POST request to delete image view')
-        return HttpResponseBadRequest('Only POST accepted')
+#@csrf_exempt
+#def multiuploader_delete(request, pk):
+    #"""
+    #View for deleting photos with multiuploader AJAX plugin.
+    #made from api on:
+    #https://github.com/blueimp/jQuery-File-Upload
+    #"""
+    #if request.method == 'POST':
+        #log.info('Called delete image. image id='+str(pk))
+        #image = get_object_or_404(MultiuploaderImage, pk=pk)
+        #image.delete()
+        #log.info('DONE. Deleted photo id='+str(pk))
+        #return HttpResponse(str(pk))
+    #else:
+        #log.info('Received not POST request to delete image view')
+        #return HttpResponseBadRequest('Only POST accepted')
 
 @csrf_exempt
-def multiuploader(request):
+def multiuploader(request, model_string):
     """
     Main Multiuploader module.
     Parses data from jQuery plugin and makes database changes.
@@ -55,38 +55,33 @@ def multiuploader(request):
 
         #writing file manually into model
         #because we don't need form of any type.
-        image = MultiuploaderImage()
-        image.filename=str(filename)
+        model = get_model(*model_string.split('.'))
+        image = model()
         image.image=file
-        image.key_data = image.key_generate
         image.save()
         log.info('File saving done')
 
         #getting thumbnail url using sorl-thumbnail
-        if 'image' in file.content_type.lower():
-            im = get_thumbnail(image, "80x80", quality=50)
-            thumb_url = im.url
-        else:
-            thumb_url = ''
+        #if 'image' in file.content_type.lower():
+            #im = get_thumbnail(image, "80x80", quality=50)
+            #thumb_url = im.url
+        #else:
 
         #settings imports
-        try:
-            file_delete_url = settings.MULTI_FILE_DELETE_URL+'/'
-            file_url = settings.MULTI_IMAGE_URL+'/'+image.key_data+'/'
-        except AttributeError:
-            file_delete_url = 'multi_delete/'
-            file_url = 'multi_image/'+image.key_data+'/'
+        #try:
+            #file_delete_url = settings.MULTI_FILE_DELETE_URL+'/'
+            #file_url = settings.MULTI_IMAGE_URL+'/'+image.key_data+'/'
+        #except AttributeError:
+            #file_delete_url = 'multi_delete/'
+            #file_url = 'multi_image/'+image.key_data+'/'
 
         #generating json response array
         result = []
-        result.append({"name":filename, 
-                       "size":file_size, 
-                       "url":file_url, 
-                       "thumbnail_url":thumb_url,
-                       "delete_url":file_delete_url+str(image.pk)+'/', 
+        result.append({"name":filename,
+                       "size":file_size,
                        "delete_type":"POST",})
         response_data = simplejson.dumps(result)
-        
+
         #checking for json data type
         #big thanks to Guy Shapiro
         if "application/json" in request.META['HTTP_ACCEPT_ENCODING']:
@@ -95,11 +90,11 @@ def multiuploader(request):
             mimetype = 'text/plain'
         return HttpResponse(response_data, mimetype=mimetype)
     else: #GET
-        return HttpResponse('Only POST accepted')
+        return HttpResponseBadRequest()
 
-def multi_show_uploaded(request, key):
-    """Simple file view helper.
-    Used to show uploaded file directly"""
-    image = get_object_or_404(MultiuploaderImage, key_data=key)
-    url = settings.MEDIA_URL+image.image.name
-    return render_to_response('multiuploader/one_image.html', {"multi_single_url":url,})
+#def multi_show_uploaded(request, key):
+    #"""Simple file view helper.
+    #Used to show uploaded file directly"""
+    #image = get_object_or_404(MultiuploaderImage, key_data=key)
+    #url = settings.MEDIA_URL+image.image.name
+    #return render_to_response('multiuploader/one_image.html', {"multi_single_url":url,})
